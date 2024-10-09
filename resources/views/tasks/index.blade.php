3b7bd686d874db1f4ffbef8edfe5a1620524babe
Truc Nguyen <trucnv@ahttech.group>
2024-10-09 14:04:59 +0700

    hotfix: change name column

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -143,8 +143,7 @@
                             <th class="py-3 px-6 text-left text-gray-700 font-medium text-center">Thời hạn hoàn thành
                             </th>
                             <th class="py-3 px-6 text-left text-gray-700 font-medium text-center">Số hiệu văn bản</th>
-                            <th style="width: 100px;" class="py-3 px-6 text-left text-gray-700 font-medium text-center">
-                                đơn vị được giao</th>
+                            <th style="width: 100px;" class="py-3 px-6 text-left text-gray-700 font-medium text-center">Số đơn vị được giao</th>
                                 <th style="width: 100px;" class="py-3 px-6 text-left text-gray-700 font-medium text-center">
                                    Tiến độ</th>
                             <th class="py-3 px-6 text-left text-gray-700 font-medium text-center">
@@ -173,8 +172,7 @@
                             </th>
                             <th class="py-3 px-6 text-left text-gray-700 font-medium text-center">Số hiệu văn bản
                             </th>
-                            <th style="width: 100px;" class="py-3 px-6 text-left text-gray-700 font-medium text-center">
-                                đơn vị được giao</th>
+                            <th style="width: 100px;" class="py-3 px-6 text-left text-gray-700 font-medium text-center">Số đơn vị được giao</th>
                                 <th style="width: 100px;" class="py-3 px-6 text-left text-gray-700 font-medium text-center">Tiến độ</th>
                             <th class="py-3 px-6 text-left text-gray-700 font-medium text-center">
                                 Chi tiết
